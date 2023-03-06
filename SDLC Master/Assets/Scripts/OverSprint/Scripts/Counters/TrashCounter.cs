4951dbb48514d9d4179d4cc55e8ba46028206338
Araiwania <51602389+dagonbo@users.noreply.github.com>
2023-03-06 20:53:17 +0700

    ResetStaticData
    รี data

--- SDLC Master/Assets/Scripts/OverSprint/Scripts/Counters/TrashCounter.cs
+++ SDLC Master/Assets/Scripts/OverSprint/Scripts/Counters/TrashCounter.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TrashCounter : BaseCounter
 {
+
+    public static event EventHandler OnAnyObjectTrashed;
+
+    new public static void ResetStaticData(){
+        OnAnyObjectTrashed = null;
+    }
     
     public override void Interact(Player player){
 
