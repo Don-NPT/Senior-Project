150177993c54f99d06186310517f5d3659114637
Araiwania <51602389+dagonbo@users.noreply.github.com>
2023-03-24 22:13:27 +0700

    จำกัดของที่หยิบขึ้นมาตามproject

--- SDLC Master/Assets/Scripts/OverSprint/Scripts/Counters/ContainerCounter.cs
+++ SDLC Master/Assets/Scripts/OverSprint/Scripts/Counters/ContainerCounter.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,37 +9,56 @@ public class ContainerCounter : BaseCounter
 
     public event EventHandler OnplayerGrabbedObject;
 
-    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] public KitchenObjectSO kitchenObjectSO;
 
+    private Project project;
+    public int numoftasksmall;
+    public int numoftaskmedium;
+    public int numoftasklarge;
+    public int numoftasksgiant;
+
+    private void Start()
+    {
+        project = ProjectManager.instance.currentProject;
+        numoftasksmall = project.smallTasks;
+        numoftaskmedium = project.mediumTasks;
+        numoftasklarge = project.largeTasks;
+        numoftasksgiant = project.giantTasks;
+    }
 
     public override void Interact(Player player)
     {
         if(!player.HasKitchenObject()){
             //ไม่ได้ถืออะไรอยู่
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefeb);
-        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-        OnplayerGrabbedObject?.Invoke(this, EventArgs.Empty);
-        }
+            if(kitchenObjectSO.objectName == "งานขนาดเล็ก" && numoftasksmall > 0){
+                numoftasksmall--;
+                Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefeb);
+                kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+                OnplayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+                Debug.Log("numoftasksmall "+ numoftasksmall);
+            }
+            if(kitchenObjectSO.objectName == "งานขนาดกลาง" && numoftaskmedium > 0){
+                numoftaskmedium--;
+                Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefeb);
+                kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+                OnplayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+                Debug.Log("numoftaskmedium "+ numoftaskmedium);
+            }
+            if(kitchenObjectSO.objectName == "งานขนาดใหญ่" && numoftasklarge > 0){
+                numoftasklarge--;
+                Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefeb);
+                kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+                OnplayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+                Debug.Log("numoftasklarge "+ numoftasklarge);
+            }
+            if(kitchenObjectSO.objectName == "งานแบบเบิ้มๆ" && numoftasksgiant > 0){
+                numoftasksgiant--;
+                Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefeb);
+                kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+                OnplayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+                Debug.Log("numoftasksgiant "+ numoftasksgiant);
+            }
     }
 
 }
-
-public class TaskCounter : BaseCounter
-{
-
-    public event EventHandler OnplayerGrabbedObject;
-
-    [SerializeField] private KitchenObjectSO kitchenObjectSO;
-
-
-    public override void Interact(Player player)
-    {
-        if(!player.HasKitchenObject()){
-            //ไม่ได้ถืออะไรอยู่
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefeb);
-        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-        OnplayerGrabbedObject?.Invoke(this, EventArgs.Empty);
-        }
-    }
-
 }
