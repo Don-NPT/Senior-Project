8b8624dd2c20efe2cca17899e62e400818d0f5dc
Araiwania <51602389+dagonbo@users.noreply.github.com>
2023-03-30 22:10:03 +0700

    Update Requirement2_new.cs

--- SDLC Master/Assets/Scripts/Developments/Requirement2_new.cs
+++ SDLC Master/Assets/Scripts/Developments/Requirement2_new.cs
@@ -7,8 +7,8 @@ using DG.Tweening;
 public class Requirement2_new : MonoBehaviour
 {
     TextMeshProUGUI TMP;
-    public int pointCorrect;
-    public int pointWrong;
+    public float pointCorrect;
+    public float pointWrong;
     public int index;
     private Project project;
 
@@ -45,8 +45,8 @@ public class Requirement2_new : MonoBehaviour
                 transform.DOScale(Vector3.one * 1.05f, 0.2f)
                     .SetEase(Ease.OutQuad)
                     .OnComplete(() => transform.localScale = Vector3.one);
-                WaterFallManager.instance.qualityEachPhase[0] += pointCorrect;
-                project.requirement2Point += pointCorrect;
+                WaterFallManager.instance.qualityEachPhase[0] += (int)Mathf.Round(pointCorrect);
+                project.requirement2Point += (int)Mathf.Round((StaffManager.instance.getSumStaffStat("Analyst")/(project.scale * 15)) * pointCorrect);
             }else{
                 FindObjectOfType<AudioManager>().Play("Warning");
                 float shakeDuration = 0.5f;
@@ -54,8 +54,8 @@ public class Requirement2_new : MonoBehaviour
                 int shakeVibrato = 10;
                 float shakeRandomness = 90.0f;
                 transform.DOShakePosition(shakeDuration, shakeStrength, shakeVibrato, shakeRandomness);
-                WaterFallManager.instance.qualityEachPhase[0] += pointWrong;
-                project.requirement2Point += pointWrong;
+                WaterFallManager.instance.qualityEachPhase[0] += (int)Mathf.Round(pointWrong);
+                project.requirement2Point += (int)Mathf.Round((StaffManager.instance.getSumStaffStat("Analyst")/(project.scale * 15)) * pointWrong);
             }
         index++;
         if(index < project.requirement2.Length )
